39999d8c19db5324b7c868b6a66a9e3230f1c8a7
amaquieira7 <agusmaqui97@gmail.com>
2026-06-11 14:24:25 -0300

    cambio Main

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-def main():
-    print("Hola Mundo")
-    print("Bienvenido a RetroPixel")
+from funciones import mostrar_menu, registrar_producto, eliminar_producto, modificar_producto, informe_general
+
+opcion = "0"
+
+while opcion != "5":
+    mostrar_menu()
+    opcion = input("Seleccione una opción: ")
+
+    if opcion == "1":
+        registrar_producto()
+    elif opcion == "2":
+        eliminar_producto()
+    elif opcion == "3":
+        modificar_producto()
+    elif opcion == "4":
+        informe_general()
+    elif opcion == "5":
+        print("Sistema finalizado.")
+    else:
+        print("Opción inválida.")
